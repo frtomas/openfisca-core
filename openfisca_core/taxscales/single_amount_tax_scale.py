0fd0b72e45d6c5740b844fa85b044f06bf1c2e52
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-04-12 15:53:03 +1000

    add numpy.interp functionality to `SingleAmountTaxScale.calc()`

--- openfisca_core/taxscales/single_amount_tax_scale.py
+++ openfisca_core/taxscales/single_amount_tax_scale.py
@@ -13,12 +13,25 @@ class SingleAmountTaxScale(AmountTaxScaleLike):
             self,
             tax_base: typing.Union[numpy.ndarray[int], numpy.ndarray[float]],
             right: bool = False,
+            interpolate: bool = False,
             ) -> numpy.ndarray[float]:
         """
         Matches the input amount to a set of brackets and returns the single cell value
         that fits within that bracket.
+
+        :arguments:
+        - `interpolate` (bool): if True, returns the linear interpolated value
+                                between the nearest two thresholds.
+
         """
-        guarded_thresholds = numpy.array([-numpy.inf] + self.thresholds + [numpy.inf])
-        bracket_indices = numpy.digitize(tax_base, guarded_thresholds, right = right)
-        guarded_amounts = numpy.array([0] + self.amounts + [0])
-        return guarded_amounts[bracket_indices - 1]
+        if interpolate:
+            thresholds = self.thresholds
+            amounts = self.amounts
+            result = numpy.interp(tax_base, thresholds, amounts)
+            return result
+
+        else:
+            guarded_thresholds = numpy.array([-numpy.inf] + self.thresholds + [numpy.inf])
+            bracket_indices = numpy.digitize(tax_base, guarded_thresholds, right = right)
+            guarded_amounts = numpy.array([0] + self.amounts + [0])
+            return guarded_amounts[bracket_indices - 1]
