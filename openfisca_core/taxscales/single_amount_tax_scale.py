756aaa8124610e15056cd7bfa581c4158cede79a
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-04-13 15:01:57 +1000

    Add keyword args 'left' and 'right' to numpy.interp() function call

--- openfisca_core/taxscales/single_amount_tax_scale.py
+++ openfisca_core/taxscales/single_amount_tax_scale.py
@@ -14,6 +14,8 @@ class SingleAmountTaxScale(AmountTaxScaleLike):
             tax_base: typing.Union[numpy.ndarray[int], numpy.ndarray[float]],
             right: bool = False,
             interpolate: bool = False,
+            interp_right: typing.Union[numpy.ndarray[int], numpy.ndarray[float], None] = None,
+            interp_left: typing.Union[numpy.ndarray[int], numpy.ndarray[float], None] = None,
             ) -> numpy.ndarray[float]:
         """
         Matches the input amount to a set of brackets and returns the single cell value
@@ -22,12 +24,14 @@ class SingleAmountTaxScale(AmountTaxScaleLike):
         :arguments:
         - `interpolate` (bool): if True, returns the linear interpolated value
                                 between the nearest two thresholds.
+        - `interp_right` (int, float): value returned in `tax_base` is greater than the highest threshold value
+        - `interp_left` (int, float): value returned in `tax_base` is less than the lowest threshold value
 
         """
         if interpolate:
             thresholds = self.thresholds
             amounts = self.amounts
-            result = numpy.interp(tax_base, thresholds, amounts)
+            result = numpy.interp(tax_base, thresholds, amounts, left=interp_left, right=interp_right)
             return result
 
         else:
