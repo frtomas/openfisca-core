55eaacc232e902dbdcb33ff04346a5cad939d05f
Mauko Quiroga <hello@mauko.me>
2021-08-30 18:08:36 +0200

    Add deprecation notice to Dummy

--- openfisca_core/commons/dummy.py
+++ openfisca_core/commons/dummy.py
@@ -2,11 +2,15 @@ import warnings
 
 
 class Dummy:
-    """A class that does nothing.
+    """A class that did nothing.
 
     Examples:
         >>> Dummy()
         <openfisca_core.commons.dummy.Dummy object...
+
+    .. deprecated:: 34.7.0
+        "The 'Dummy' class has been deprecated since version 34.7.0,",
+        "and it will be removed in version 36.0.0.",
 
     """
 
