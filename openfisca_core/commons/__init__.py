08162fe808123e534230221c9d121d469ed9a532
Mauko Quiroga <hello@mauko.me>
2021-08-31 23:31:45 +0200

    Add traits to avoid cyclic imports

--- openfisca_core/commons/__init__.py
+++ openfisca_core/commons/__init__.py
@@ -22,7 +22,8 @@
 # See: https://www.python.org/dev/peps/pep-0008/#imports
 
 from .typing import ArrayLike, Array  # noqa: F401
+from .dummy import Dummy  # noqa: F401
 from .formulas import apply_thresholds, concat, switch  # noqa: F401
 from .misc import empty_clone, stringify_array  # noqa: F401
 from .rates import average_rate, marginal_rate  # noqa: F401
-from .dummy import Dummy  # noqa: F401
+from .traits import Personifiable, Rolifiable  # noqa: F401
---
+++ openfisca_core/commons/traits.py
@@ -0,0 +1,27 @@
+import abc
+import dataclasses
+import textwrap
+
+
+class Documentable(abc.ABC):
+
+    @property
+    def doc(self) -> str:
+        return self.__doc
+
+    @doc.setter
+    def doc(self, value: str) -> None:
+        self.__doc = textwrap.dedent(value)
+
+
+@dataclasses.dataclass(repr = False)
+class Personifiable(Documentable, abc.ABC):
+    key: str
+    plural: str
+    label: str
+    doc: str
+
+
+@dataclasses.dataclass(repr = False)
+class Rolifiable(Personifiable, abc.ABC):
+    ...
