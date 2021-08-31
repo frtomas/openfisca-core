524d2536e242cc4133f3782873cfa11df284b6e5
Mauko Quiroga <hello@mauko.me>
2021-08-31 12:04:53 +0200

    Add nptyping for clearer numpy types

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ general_requirements = [
     'dpath >= 1.5.0, < 2.0.0',
     'pytest >= 4.4.1, < 6.0.0',  # For openfisca test
     'numpy >= 1.11, < 1.21',
+    'nptyping >= 1.4.3, < 2.0.0',
     'psutil >= 5.4.7, < 6.0.0',
     'PyYAML >= 3.10',
     'sortedcontainers == 2.2.2',
