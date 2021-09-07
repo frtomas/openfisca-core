17ba088eefc895f015329b1f64d1ca44996bbb35
Mauko Quiroga <hello@mauko.me>
2021-09-07 19:31:47 +0200

    Add doc to role builder

--- openfisca_core/entities/role_builder.py
+++ openfisca_core/entities/role_builder.py
@@ -6,6 +6,39 @@ from openfisca_core.types import Personifiable, Rolifiable, RoleLike
 
 
 class RoleBuilder:
+    """Builds roles & sub-roles from a given input.
+
+    Attributes:
+        builder (:obj:`Personifiable`):
+            A builder object, like a :class:`.GroupEntity` for example.
+        buildee (:obj:`Rolifiable`):
+            The objects to be built, in this case :class:`.Role`.
+
+    Args:
+        builder: A builder object, like a :class:`.GroupEntity` for example.
+        buildee: The objects to be built, in this case :class:`.Role`.
+
+    Examples:
+        >>> from openfisca_core.entities import GroupEntity, Role
+
+        >>> group_entity = GroupEntity(
+        ...     "household",
+        ...     "households",
+        ...     "A household",
+        ...     "All the people who live together in the same place.",
+        ...     []
+        ...    )
+
+        >>> items = [{
+        ...     "key": "parent",
+        ...     "subroles": ["first_parent", "second_parent"],
+        ...     }]
+
+        >>> builder = RoleBuilder(group_entity, Role)
+        >>> builder(items)
+        [Role(parent)]
+
+    """
 
     def __init__(
             self,
@@ -16,9 +49,29 @@ class RoleBuilder:
         self.buildee = buildee
 
     def __call__(self, items: Iterable[RoleLike]) -> Sequence[Rolifiable]:
+        """Builds a sub/role for each item in ``items``.
+
+        Args:
+            items: Role-like items, see :class:`.RoleLike`.
+
+        Returns:
+            A :obj:`list` of :obj:`.Role`.
+
+        """
+
         return [self.build(item) for item in items]
 
     def build(self, item: RoleLike) -> Rolifiable:
+        """Builds a role from ``item``.
+
+        Args:
+            item: A role-like item, see :class:`.RoleLike`.
+
+        Returns:
+            A role.
+
+        """
+
         role = self.buildee(item, self.builder)
         self.builder.__dict__[role.key.upper()] = role
         subroles = item.get("subroles", [])
