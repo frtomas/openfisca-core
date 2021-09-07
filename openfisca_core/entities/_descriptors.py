b3c6bf95c6d87d1ae81dbba52a24cfbf2a2df7fe
Mauko Quiroga <hello@mauko.me>
2021-09-07 17:02:16 +0200

    Fix types in descriptor

--- openfisca_core/entities/_descriptors.py
+++ openfisca_core/entities/_descriptors.py
@@ -4,19 +4,19 @@ from typing import Any, Callable, Optional, Type
 
 from openfisca_core.variables import Variable
 
-FuncType = Callable[[str, bool], Optional[Variable]]
+_F = Callable[[str, bool], Optional[Variable]]
 
 
 class VariableDescriptor:
     public_name: str = "variable"
     private_name: str = "_variable"
 
-    def __get__(self, obj: Any, type: Type[Any]) -> Optional[FuncType]:
+    def __get__(self, obj: Any, type: Type[Any]) -> Optional[_F]:
 
-        func: Optional[FuncType]
+        func: Optional[_F]
         func = getattr(obj, self.private_name, None)
         return func
 
-    def __set__(self, obj: Any, value: FuncType) -> None:
+    def __set__(self, obj: Any, value: _F) -> None:
 
         setattr(obj, self.private_name, value)
--- openfisca_core/types/protocols/descriptable.py
+++ openfisca_core/types/protocols/descriptable.py
@@ -2,16 +2,16 @@ from typing import Any, Callable, Optional, Type, TypeVar
 
 from typing_extensions import Protocol
 
-T = TypeVar("T", covariant = True)
-F = Callable[..., Optional[T]]
+_T = TypeVar("_T")
+_F = Callable[..., Optional[_T]]
 
 
-class Descriptable(Protocol[T]):
+class Descriptable(Protocol[_T]):
     public_name: str
     private_name: str
 
-    def __get__(self, obj: Any, type: Type[Any]) -> Optional[F]:
+    def __get__(self, obj: Any, type: Type[Any]) -> Optional[_F[_T]]:
         ...
 
-    def __set__(self, obj: Any, value: F) -> None:
+    def __set__(self, obj: Any, value: _F[_T]) -> None:
         ...
