6e1e1a733941be286be59210475bbfd3cecc79c5
Mauko Quiroga <hello@mauko.me>
2021-09-07 17:47:46 +0200

    Add doc to protocol aggregatable

--- openfisca_core/types/protocols/_aggregatable.py
+++ openfisca_core/types/protocols/_aggregatable.py
@@ -2,4 +2,12 @@ from typing_extensions import Protocol
 
 
 class Aggregatable(Protocol):
+    """Base type for any model implementing population-like behaviour.
+
+    Type-checking against abstractions rather than implementations helps in
+    (a) decoupling the codebse, thanks to structural subtyping, and
+    (b) documenting/enforcing the blueprints of the different OpenFisca models.
+
+    """
+
     ...
