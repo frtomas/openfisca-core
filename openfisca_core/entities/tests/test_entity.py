fc87b4b5ec5f39c07f3b71a48322d01dcbf724bc
Mauko Quiroga <hello@mauko.me>
2021-09-07 14:30:46 +0200

    Add deprecation test to set tax-benefit system

--- openfisca_core/entities/tests/test_entity.py
+++ openfisca_core/entities/tests/test_entity.py
@@ -1,13 +1,28 @@
 import pytest
 
 from openfisca_core.entities import Entity, Role
+from openfisca_core.taxbenefitsystems import TaxBenefitSystem
 
 
-def test_check_role_validity_deprecation():
-    """check_role_validity() throws a deprecation warning when used."""
+@pytest.fixture
+def entity():
+    return Entity("key", "label", "plural", "doc")
 
-    entity = Entity("key", "label", "plural", "doc")
-    role = Role({"key": "key"}, entity)
+
+@pytest.fixture
+def role(entity):
+    return Role({"key": "key"}, entity)
+
+
+def test_set_tax_benefit_system_deprecation(entity):
+    """:meth:`.set_tax_benefit_system` throws a deprecation warning."""
+
+    with pytest.warns(DeprecationWarning):
+        entity.set_tax_benefit_system(TaxBenefitSystem([entity]))
+
+
+def test_check_role_validity_deprecation(entity, role):
+    """:meth:`.check_role_validity` throws a deprecation warning."""
 
     with pytest.warns(DeprecationWarning):
         entity.check_role_validity(role)
