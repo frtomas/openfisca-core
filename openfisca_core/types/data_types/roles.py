a9b682ee539ba560cd8874b0f48a599861a62bd2
Mauko Quiroga <hello@mauko.me>
2021-09-07 17:49:43 +0200

    Add doc to types roles

--- openfisca_core/types/data_types/roles.py
+++ openfisca_core/types/data_types/roles.py
@@ -3,6 +3,14 @@ from typing_extensions import TypedDict
 
 
 class RoleLike(TypedDict, total = False):
+    """Base type for any data castable to a role-like model.
+
+    Type-checking against abstractions rather than implementations helps in
+    (a) decoupling the codebse, thanks to structural subtyping, and
+    (b) documenting/enforcing the blueprints of the different OpenFisca models.
+
+    """
+
     key: str
     plural: Optional[str]
     label: Optional[str]
