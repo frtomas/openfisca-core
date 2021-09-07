6ac6e5e92f46051dea5b9961ee8e77479097af36
Mauko Quiroga <hello@mauko.me>
2021-09-07 18:08:22 +0200

    Add doc to deprecated decorator

--- openfisca_core/commons/decorators.py
+++ openfisca_core/commons/decorators.py
@@ -3,16 +3,54 @@ import warnings
 import typing
 from typing import Any, Callable, TypeVar
 
-_F = TypeVar("_F", bound = Callable[..., Any])
+F = TypeVar("F", bound = Callable[..., Any])
 
 
 class deprecated:
+    """Allows (soft) deprecating a functionality of OpenFisca.
+
+    Attributes:
+        since (:obj:`str`): Since when the functionality is deprecated.
+        expires (:obj:`str`): When wil it be removed forever?
+
+    Args:
+        since: Since when the functionality is deprecated.
+        expires: When wil it be removed forever?
+
+    Examples:
+        >>> @deprecated(since = "35.5.0", expires = "in the future")
+        ... def obsolete():
+        ...     return "I'm obsolete!"
+
+    """
 
     def __init__(self, since: str, expires: str) -> None:
         self.since = since
         self.expires = expires
 
-    def __call__(self, function: _F) -> _F:
+    def __call__(self, function: F) -> F:
+        """Wraps a function to return another one, decorated.
+
+        Args:
+            function: The function or method to decorate.
+
+        Returns:
+            The decorated function.
+
+        Examples:
+            >>> def obsolete():
+            ...     return "I'm obsolete!"
+
+            >>> decorator = deprecated(
+            ...     since = "35.5.0",
+            ...     expires = "in the future",
+            ...     )
+
+            >>> decorator(obsolete)
+            <function obsolete ...>
+
+        """
+
         self.function = function
 
         def wrapper(*args: Any, **kwds: Any) -> Any:
@@ -25,4 +63,4 @@ class deprecated:
             return self.function(*args, **kwds)
 
         functools.update_wrapper(wrapper, function)
-        return typing.cast(_F, wrapper)
+        return typing.cast(F, wrapper)
