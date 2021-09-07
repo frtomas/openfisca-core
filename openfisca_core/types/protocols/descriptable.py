6420eca32028aa9db24fe12cacb7502add7e2da8
Mauko Quiroga <hello@mauko.me>
2021-09-07 17:34:04 +0200

    Add doc to protocol descriptable

--- openfisca_core/types/protocols/descriptable.py
+++ openfisca_core/types/protocols/descriptable.py
@@ -2,16 +2,24 @@ from typing import Any, Callable, Optional, Type, TypeVar
 
 from typing_extensions import Protocol
 
-_T = TypeVar("_T")
-_F = Callable[..., Optional[_T]]
+T = TypeVar("T")
+F = Callable[..., Optional[T]]
 
 
-class Descriptable(Protocol[_T]):
+class Descriptable(Protocol[T]):
+    """Base type for any model implementing a descriptor.
+
+    Type-checking against abstractions rather than implementations helps in
+    (a) decoupling the codebse, thanks to structural subtyping, and
+    (b) documenting/enforcing the blueprints of the different OpenFisca models.
+
+    """
+
     public_name: str
     private_name: str
 
-    def __get__(self, obj: Any, type: Type[Any]) -> Optional[_F[_T]]:
+    def __get__(self, obj: Any, type: Type[Any]) -> Optional[F[T]]:
         ...
 
-    def __set__(self, obj: Any, value: _F[_T]) -> None:
+    def __set__(self, obj: Any, value: F[T]) -> None:
         ...
