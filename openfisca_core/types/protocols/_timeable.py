aa916731135446f5197f446a71bcdf8b00d2fa5a
Mauko Quiroga <hello@mauko.me>
2021-09-07 17:44:16 +0200

    Add doc to protocol timeable

--- openfisca_core/types/protocols/_timeable.py
+++ openfisca_core/types/protocols/_timeable.py
@@ -2,4 +2,12 @@ from typing_extensions import Protocol
 
 
 class Timeable(Protocol):
+    """Base type for any model implementing a period/instant-like behaviour.
+
+    Type-checking against abstractions rather than implementations helps in
+    (a) decoupling the codebse, thanks to structural subtyping, and
+    (b) documenting/enforcing the blueprints of the different OpenFisca models.
+
+    """
+
     ...
